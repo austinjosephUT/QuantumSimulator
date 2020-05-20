dd319db18c4dd38fcd14cd2977faf39d2fa20bbd
Austin Joseph <austinjoseph@utexas.edu>
2020-05-19 22:11:22 -0500

    added basic interface for data analytics

---
+++ noise.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class NoisyGates:
+
+    def __init__(self):
+        self.y = 3
+        y = 0
+        #apply gate
+
+    def applydecoherence(self):
+        pass
---
+++ userinterface.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+class UserInterface:
+
+    def __init__(self):
+        window = Tk()
+        self.openwindow()
+
+    def openwindow(self):
+        window = Tk()
+        window.title("Quantum Simulator")
+        window.mainloop()
+
+    def addButton(self):
+        pass
